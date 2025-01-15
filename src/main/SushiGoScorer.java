fedb7aed90ade8c49f219c12bff25c04b8913c89
fhaji003 <fhaji003@mtroyal.ca>
2025-01-15 19:26:34 +0000

    SushiGoScorer

--- src/main/SushiGoScorer.java
+++ src/main/SushiGoScorer.java
@@ -7,19 +7,32 @@ class SushiGoScorer {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
 
-        // Prompt for and get user's name.
+        String userName;
+        System.out.println("Enter your name: ");
+        userName= keyboard.nextLine();
+        System.out.println("========================");
+
+
 
         // Prompt for and get the number of tempura cards.
         // Calculate the tempura card score.
+        int tempuraCards;
+        System.out.print("Enter the number of tempura cards: ");
+        tempuraCards= keyboard.nextInt();
+        int tempuraPoints= 0;
 
-        System.out.print("How many tempura cards do you have? "); // Why use print() instead of println() here?
-        int numDumplings = keyboard.nextInt();
-
+        if (tempuraCards <2) {
+            tempuraPoints = 0;
+        } else if (tempuraCards>=2 && tempuraCards %2==0) {
+            tempuraPoints = (tempuraCards * 2) +1;
+        }
         // One way to calculate the score.
         // There are others you should explore.
         // If you want to be clever, you could explore Triangular Numbers
         // (https://bit.ly/4fVQhVp) and
         // Java's Math.min() or Math.max() built-in functions (https://bit.ly/3CkHL4t).
+        System.out.print("How many dumpling cards do you have? "); // Why use print() instead of println() here?
+        int numDumplings = keyboard.nextInt();
         int dumplingPoints = 0;
 
         if (numDumplings >= 5) {
@@ -35,6 +48,10 @@ class SushiGoScorer {
         } else {
             dumplingPoints = 0;
         }
+
+
+        System.out.printf("Tempura: %3d%n", tempuraPoints);
+        System.out.printf("Dumplings: %2d%n", dumplingPoints);
 
         // Output the results using System.out.printf().
         // Make the resulting table look like this:
